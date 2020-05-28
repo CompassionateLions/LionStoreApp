71a212362088ed4cd2607ea083b879531a96754f
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-05-28 22:04:26 +0930

    Getting structure together with vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <StoreHeader/>
+    <StoreSideBar v-bind:catagories="catagories"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StoreHeader from './components/StoreHeader.vue'
+import StoreSideBar from './components/StoreSideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StoreHeader,
+    StoreSideBar
+  },
+  data(){
+    return {
+      catagories: [
+        {
+          name: "Drama",
+          id: 1
+        },
+        {
+          name: "Horror",
+          id: 2
+        },
+        {
+          name: "Thriller",
+          id: 3
+        },
+        {
+          name: "Action",
+          id: 4
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/CatagoryItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <li>{{catagory.name}}</li>
+</template>
+
+<script>
+    export default {
+        name: "CatagoryItem",
+        props: ["catagory"]
+    }
+</script>
+
+<style scoped>
+
+</style>
