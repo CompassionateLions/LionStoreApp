4c7ad3271d618cff20dbc01b53f0f38190864d0e
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-05-30 14:09:54 +0930

    Getting sidebar and header together

--- src/components/StoreSideBar.vue
+++ src/components/StoreSideBar.vue
@@ -1,29 +1,30 @@
 <template>
-    <div class="sidebar">
-        <ul>
-            <div v-bind:key="catagory.id" v-for="catagory in catagories">
-                <CatagoryItem  v-bind:catagory="catagory"/>
-            </div>
-        </ul>
-    </div>
+  <div class="sidebar">
+    <ul id="slide-out" class="sidenav sidenav-fixed">
+      <div v-bind:key="catagory.id" v-for="catagory in catagories">
+        <CatagoryItem v-bind:catagory="catagory" />
+      </div>
+    </ul>
+    <a href="#" data-target="slide-out" class="sidenav-trigger">
+      <i class="material-icons">menu</i>
+    </a>
+  </div>
 </template>
 
 <script>
-import CatagoryItem from './CatagoryItem.vue'
+import CatagoryItem from "./CatagoryItem.vue";
 
-    export default {
-        name: "StoreSideBar",
-        props: ["catagories"],
-        components: {
-            CatagoryItem
-        }
-    }
+export default {
+  name: "StoreSideBar",
+  props: ["catagories"],
+  components: {
+    CatagoryItem
+  }
+};
 </script>
 
 <style  scoped>
-.sidebar {
-    height: 100%;
-    background-color: orangered;
-    width: 100px;
+.sidenav-fixed {
+    padding-top: 64px;
 }
 </style>
