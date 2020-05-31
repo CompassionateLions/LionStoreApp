fb3206d9d3f7f2ec49c0313560963ed4e9f49995
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-06-01 04:02:34 +0930

    Added year filter and fixed search

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -4,11 +4,17 @@
       <div class="nav-wrapper orange darken-3">
         <form>
           <div class="input-field">
-            <input id="search" type="search" required />
+            <input 
+            id="search" 
+            class="white-text" 
+            type="search" 
+            placeholder="Search"
+            v-model="searchTerm"
+            v-on:keyup="searchTitle" />
             <label class="label-icon" for="search">
-              <i class="material-icons">search</i>
+              <i class="material-icons search-icon">search</i>
             </label>
-            <i class="material-icons">close</i>
+            <i class="material-icons close-icon">close</i>
           </div>
         </form>
       </div>
@@ -18,9 +24,44 @@
 
 <script>
 export default {
-  name: "SearchBox"
+  name: "SearchBox",
+  data() {
+      return {
+          searchTerm: ''
+      }
+  },
+  methods: {
+      searchTitle() {
+          console.log(this.searchTerm);
+      }
+  }
 };
 </script>
 
 <style  scoped>
+
+.search-wrapper {
+    position: relative;
+}
+
+.input-field {
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
+
+#search {
+    margin: 0;
+}
+
+#search:focus {
+    color: black!important;
+}
+
+.search-icon,.close-icon {
+    position: absolute;
+    top: 24px!important;
+    transform: translate(0, -50%);
+}
+
+
 </style>
