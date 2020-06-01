f122a48ae8fc8d53bdabfb62a838d430ae211ea5
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-06-02 04:22:28 +0930

    changed models slightly

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize, DataTypes) {
             },
             password: {
                 type: DataTypes.STRING
-            },
+            }
         }
     );
 
