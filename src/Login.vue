3d72e1571aa555625b4daf1c1434782765d9837f
Ziyen <zyloh89@gmail.com>
2020-06-03 19:21:49 +0930

    draft login form

--- src/Login.vue
+++ src/Login.vue
@@ -1,12 +1,43 @@
 <template>
-    <div>
-        <h1>Login</h1>
+    <div id="app">
+    <StoreHeader/>
+        <div class="row">
+            <div class ="col s12 m4 offset-m4">
+                <div class="card">
+                    <div class ="card-action">
+                        <h3>Login Form</h3>
+                    </div>
+                    <div class ="card-content">
+                        <div class="form-field">
+                            <label for="email">Email</label>
+                            <input type="text" placeholder="Email" id="username">
+                        </div><br>
+
+                        <div class="form-field">
+                            <label for="password">Password</label>
+                            <input type="text"  placeholder="Password" id="password">
+                        </div><br>
+
+                        <div class="form-field">
+                            <button class="btn-medium" style="width:50%;" id="login-btn">Login</button>
+                            <button class="btn-medium" style="width:50%;" id="clear-btn">Clear</button><br>
+                            <button class="btn-medium" style="width:100%;" id="signup-btn">Sign Up</button>
+                        </div><br>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import StoreHeader from './components/StoreHeader.vue'
+
+export default {
+    name: 'Login',
+    components: {
+    StoreHeader  
+  },  
     }
 </script>
 
