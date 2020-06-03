0e580490e35f77b03ded4c6c6fd104bfa6a736fb
Ruma Das <rumaadl16@gmail.com>
2020-06-03 19:25:58 +0930

    Push cart file

--- src/Cart.vue
+++ src/Cart.vue
@@ -6,43 +6,10 @@
         <table>
           <tr class="collection-item">
             <div two-line>
-              <template class="collection " v-for="(product, index) in products">
-                <div v-bind:key="product.product_id" avatar>
-                  <td>
-                    <img avatar :src="product.photo" />
-                    <img />
-                  </td>
-                  <td>
-                    <li v-html="product.name"></li>
-                  </td>
-                  <td>
-                    <li class="side_padd"><b>Price</b></li>
-                    <li>{{product.price}}$</li>
-                  </td>
-                  <td>
-                    <div class="side_padd">
-                      <li><b>Quantity</b></li>
-                      <li><a href="#!" class="secondary-content">
-                      <i class="material-icons">expand_less</i>
-                    </a></li>
-                      <li  class="border_style" label="Quantity" v-html="product.qty"></li>
-                      <li><i class="material-icons">expand_more</i></li>
-                    </div>
-                  </td>
-                  <td>
-                    <div  class="side_padd">
-                      <li class="side_padd"><b>Total Price</b></li>
-                      <li>{{product.price * product.qty}}$</li>
-                    </div>
-                  </td>
-                  <td>
-                    <a href="#!" class="secondary-content">
-                      <i class="material-icons">delete</i>
-                    </a>
-                  </td>
-                </div>
-                <div  v-if="index + 1 < products.length" v-bind:key="index"><hr></div>
-              </template>
+              <div class="collection " v-bind:key="product.product_id" v-for="(product, index) in products">
+               <CartItem v-bind:product="product" v-bind:index="index" />
+                     <div><hr></div>
+              </div>
             </div>
           </tr>
         </table>
@@ -52,7 +19,10 @@
 </template>
 
 <script>
+import CartItem from "./components/CartItem"
 export default {
+  name: "Cart", 
+  components: {CartItem},
   data: () => ({
     products: [
       {
@@ -73,6 +43,8 @@ export default {
         price: 14.99,
         qty: 2
       }
+  
+
    
     ]
   })
@@ -80,24 +52,5 @@ export default {
 </script>
 
 <style scoped>
-i {
-  color: #f44336;
-}
-img {
-  height: 120px;
-  width: 130px;
-}
-img:hover {
-  cursor: pointer;
-}
-li {
-  list-style: none;
-  padding-left: 10px;
-}
-.border_style{
-    border-style: groove;
-}
-.side_padd{
-  padding-left: 20px;
-}
+
 </style>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+      <div>
+                  <td>
+                    <img avatar :src="product.photo" />
+                  </td>
+                  <td>
+                    <li v-html="product.name"></li>
+                  </td>
+                  <td>
+                    <li class="side_padd"><b>Price</b></li>
+                    <li>{{product.price}}$</li>
+                  </td>
+                  <td>
+                    <div class="side_padd">
+                      <li><b>Quantity</b></li>
+
+                     <li><a><i class="material-icons">expand_less</i></a></li>
+                  
+                      <li  class="border_style" label="Quantity" v-html="product.qty"></li>
+                      <li><i class="material-icons">expand_more</i></li>
+                    </div>
+                  </td>
+                  <td>
+                    <div  class="side_padd">
+                      <li class="side_padd"><b>Total Price</b></li>
+                      <li>{{product.price * product.qty}}$</li>
+                    </div>
+                  </td>
+                  <td>
+                    <a href="#!" class="secondary-content">
+                      <i class="material-icons">delete</i>
+                    </a>
+                  </td>
+                </div>         
+  </div>
+</template>
+<script>
+export default {
+    name: "CartItem",
+    props: [
+        "product",
+        "index"
+    ]
+}
+</script>
+
+<style scoped>
+i {
+  color: #f44336;
+}
+img {
+  height: 120px;
+  width: 130px;
+}
+img:hover {
+  cursor: pointer;
+}
+li {
+  list-style: none;
+  padding-left: 10px;
+}
+.border_style{
+    border-style: groove;
+}
+.side_padd{
+  padding-left: 20px;
+}
+</style>
