b2d5997e858e0c110094638dad62c370542cf5d4
Ruma Das <rumaadl16@gmail.com>
2020-06-04 23:16:09 +0930

    changing css in cart

--- src/Cart.vue
+++ src/Cart.vue
@@ -4,7 +4,7 @@
    
     <div class="row">
         <div class="col l6 s12 offset-l3 ">
-       <h4>Items added to your Shopping <span class="cartSty">Cart</span>  </h4>
+       <h4>Items added to your Shopping <span class="">Cart</span>  </h4>
         </div>
       <div class="col l6 s12 offset-l3 border_style z-depth-2">
         <div class="col m12 s12 ">
@@ -15,13 +15,13 @@
         
         </div>
         <div>
-          <div class="collection" v-bind:key="product.productId" v-for="(product) in products">
+          <div class="collect" v-bind:key="product.productId" v-for="(product) in products">
             <CartItem v-bind:product="product" />
           </div>
         </div>
         <div class="col l3 s6 offset-l9 -s6">
            <router-link to="/login">
-           <button class="btn waves-effect waves-light orange darken-3" type="submit" name="action">
+           <button class="btn waves-effect waves-light   deep-orange darken-3" type="submit" name="action">
             Submit
           </button>
            </router-link>        
@@ -78,18 +78,28 @@ export default {
 
 <style scoped>
 .border_style {
-  padding: 10px;
+  padding:15px;
   margin-top: 30px;
 }
 i {
-  color: #f44336;
-  font-size: 40px;
+  color: #bf360c;
   padding-top: 10;
 }
 .cartSty{
   font-size: 50px;
-    color: #f44336;
+    color: #bf360c;
     font-weight: bold;
     font-family:Verdana, Geneva, Tahoma, sans-serif;
+}
+ .collect{
+ 
+    border: 1px solid #e0e0e0;
+    border-radius: 2px;
+    overflow: hidden;
+    position: relative;
+    border-block-end-style: none;
+}
+button{
+ margin-top: 10px;
 }
 </style>
--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,48 +1,46 @@
 <template>
-  <div>
-    <div class="row divStyle">
-      <div class="col l2 m12 ">
-        <li>
-          <a>
-            <img class="responsive-img" :src="product.image_url" />
-          </a>
-        </li>
-      </div>
-      <div class="col l4 m12 textStyle">
-        <h6 v-html="product.name" class="red-text text-darken-3"></h6>
-        <li v-html="product.format"></li>
-        <li>
-          <b>Price</b>
-        </li>
-        <li>${{product.price}}</li>
-      </div>
-      <div class="col l2 m12  textStyle">
-        <li>
-          <b>Quantity</b>
-        </li>
-        <li class="padL">
-          <a>
-            <i class="material-icons">expand_less</i>
-          </a>
-        </li>
-        <li class="qty" label="Quantity" v-html="product.quantity"></li>
-        <li class="padL">
-          <a>
-            <i class="material-icons">expand_more</i>
-          </a>
-        </li>
-      </div>
-      <div class="col l2 m12  s12textStyle">
-        <li>
-          <b>Total Price</b>
-        </li>
-        <li>${{product.price * product.quantity}}</li>
-      </div>
-      <div class="col l2 m12 s12 offset-l1 divStyle">
-        <a class="secondary-content ">
-          <i class="material-icons ">delete</i>
+  <div class="row divStyle">
+    <div class="col l2 m12">
+      <li>
+        <a>
+          <img class="responsive-img" :src="product.image_url" />
         </a>
-      </div>
+      </li>
+    </div>
+    <div class="col l4 m12 textStyle">
+      <li v-html="product.name" class="red-text text-darken-3 movName"></li>
+      <li v-html="product.format"></li>
+      <li>
+        <b>Price</b>
+      </li>
+      <li>${{product.price}}</li>
+    </div>
+    <div class="col l2 m12 textStyle">
+      <li>
+        <b>Quantity</b>
+      </li>
+      <li class="padL">
+        <a>
+          <i class="material-icons">expand_less</i>
+        </a>
+      </li>
+      <li class="qty" label="Quantity" v-html="product.quantity"></li>
+      <li class="padL">
+        <a>
+          <i class="material-icons">expand_more</i>
+        </a>
+      </li>
+    </div>
+    <div class="col l2 m12 s12 textStyle">
+      <li>
+        <b>Total Price</b>
+      </li>
+      <li>${{product.price * product.quantity}}</li>
+    </div>
+    <div class="col l2 m12 s12 offset-l1 divStyle">
+      <a class="secondary-content">
+        <i class="material-icons">delete</i>
+      </a>
     </div>
   </div>
 </template>
@@ -55,10 +53,13 @@ export default {
 
 <style scoped>
 i {
-  color: #f44336;
+  color: #bf360c;
 }
-img {
+.movName {
+  font-weight: bold;
+  font-size: 14px;
 }
+
 img:hover {
   cursor: pointer;
 }
@@ -68,12 +69,10 @@ li {
 }
 .border_style {
   border-style: groove;
-  padding: 5px;
 }
 .divStyle {
-  padding-top: 20px;
+  padding-top: 10px;
   padding-left: 10px;
-  margin: 10px;
 }
 .binStyle {
   padding-top: 60%;
@@ -83,6 +82,7 @@ li {
   text-align: left;
   padding-left: 20px;
   margin-left: 20px;
+  font-size: 14px;
 }
 a {
   cursor: pointer;
@@ -90,7 +90,7 @@ a {
 .qty {
   text-align: center;
   padding: 3px;
-  border: 2px solid #f44336;
+  border: 2px solid #bf360c;
   width: 40px;
   height: 40px;
   margin-left: 10px;
