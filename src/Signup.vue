8bbe3df357cfed17b92fc0ac41570f1a2339b410
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-06-09 06:36:00 +0930

    Signup layout

--- src/Signup.vue
+++ src/Signup.vue
@@ -1,48 +1,76 @@
 <template>
-    <div id="app">
-    <StoreHeader/>
-        <div class="row">
-            <div class ="col s12 m4 offset-m4">
-                <div class="card">
-                    <div class ="card-action">
-                        <h3>Sign Up</h3>
-                    </div>
-                    <div class ="card-content">
-                        <div class="form-field">
-                            <label for="email">Email</label>
-                            <input type="text" placeholder="Email" id="username">
-                        </div><br>
-
-                        <div class="form-field">
-                            <label for="password">Password</label>
-                            <input type="text"  placeholder="Password" id="password">
-                        </div><br>
-
-                         <div class="row">
-                            <a class="btn-large waves-effect waves-light orange darken-3" style="width:50%;" type ="submit" id="login-btn">Register</a>
-                            <a class="btn-large waves-effect waves-light orange darken-3" style="width:50%;" id="clear-btn">Clear</a><br>
-                        </div>
-                        <div class="row">
-                            <router-link to="/Login"><a class="btn-large waves-effect waves-light orange darken-3" style="width:100%;" id="signup-btn">Login</a></router-link>
-                        </div><br>
-                    </div>
-                </div>
+  <div id="app">
+    <StoreHeader />
+    <div class="container">
+      <div class="row signup-container">
+        <div class="col s12 m10 l8">
+          <div class="card">
+            <div class="card-action">
+              <h3>Sign Up</h3>
             </div>
+            <div class="card-content">
+              <div class="form-field">
+                <label for="email">Email</label>
+                <input type="text" placeholder="Email" id="username" />
+              </div>
+              <br />
+
+              <div class="form-field">
+                <label for="password">Password</label>
+                <input type="text" placeholder="Password" id="password" />
+              </div>
+              <br />
+
+              <div class="row">
+                <a
+                  class="btn-large waves-effect waves-light orange darken-3"
+                  style="width:50%;"
+                  type="submit"
+                  id="login-btn"
+                >Register</a>
+                <a
+                  class="btn-large waves-effect waves-light orange darken-3"
+                  style="width:50%;"
+                  id="clear-btn"
+                >Clear</a>
+                <br />
+              </div>
+              <div class="row">
+                <router-link to="/Login">
+                  <a
+                    class="btn-large waves-effect waves-light orange darken-3"
+                    style="width:100%;"
+                    id="signup-btn"
+                  >Login</a>
+                </router-link>
+              </div>
+              <br />
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import StoreHeader from './components/StoreHeader.vue'
+import StoreHeader from "./components/StoreHeader.vue";
 
 export default {
-    name: 'Signup',
-    components: {
-    StoreHeader  
-  },  
-    }
+  name: "Signup",
+  components: {
+    StoreHeader
+  }
+};
 </script>
 
 <style scoped>
+.signup-container {
+  display: flex;
+  justify-content: center;
+}
 
+.col {
+  margin: 0 !important;
+}
 </style>
